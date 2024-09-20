53ed27ffe2cf71c7e6eca4de2645b96c89ffafac
Elaine Muhombe <emuhombe@gmail.com>
2024-09-20 11:42:15 +0300

    feat: Implement HomePage with Layout component and header content

--- client/src/components/pages/HomePage.tsx
+++ client/src/components/pages/HomePage.tsx
@@ -1,8 +1,17 @@
-
 // src/components/pages/HomePage.tsx
+import Layout from "../layout/Layout";
+
 export default function HomePage() {
   return (
     <div>
+      <Layout>
+      <header className="text-center mb-8 px-4 md:px-8 lg:px-16">
+      <h1 className="text-4xl font-bold mb-2">Welcome to Berozgaar</h1>
+      <p className="text-lg">
+        Your ultimate destination for job listings and career opportunities
+      </p>
+    </header>
+      </Layout>
       
     </div>
   )
