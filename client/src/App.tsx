e7ff1c00404160f6d3beb450364f4216dc0d3003
Elaine Muhombe <emuhombe@gmail.com>
2024-09-21 16:50:34 +0300

    refactor: Edit comments in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,33 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import routing components from react-router-dom
-import Login from "./components/pages/Login"; // Import Login component
-import Register from "./components/pages/Register"; // Import Register component
-import JobList from "./components/jobs/JobList"; // Import JobList component
-import JobDetails from "./components/jobs/JobDetail"; // Import JobDetails component
-import JobSeekerDashboard from "./components/dashboard/JobSeekerDashboard"; // Import JobSeekerDashboard component
-import EmployerDashboard from "./components/dashboard/EmployerDashBoard"; // Import EmployerDashboard component
-import AuthProvider from "./components/context/AuthContext"; // Import AuthProvider for authentication context
-import PrivateRoute from "./components/routes/PrivateRoutes"; // Import PrivateRoute for protected routes
-import HomePage from "./components/pages/HomePage"; // Import HomePage component
-import "./App.css"; // Import App-level CSS
-import "./index.css"; // Import global styles
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import JobList from "./components/jobs/JobList";
+import JobDetails from "./components/jobs/JobDetail";
+import JobSeekerDashboard from "./components/dashboard/JobSeekerDashboard";
+import EmployerDashboard from "./components/dashboard/EmployerDashBoard";
+import AuthProvider from "./components/context/AuthContext";
+import PrivateRoute from "./components/routes/PrivateRoutes";
+import HomePage from "./components/pages/HomePage";
+import "./App.css";
+import "./index.css";
 
 const App = () => {
   return (
-    <AuthProvider> {/* Provide authentication context to the application */}
-      <Router> {/* Set up routing for the application */}
-        <Routes> {/* Define all the application routes */}
-          <Route path="/" element={<HomePage />} /> {/* Define a route for the root path */}
-          <Route path="/login" element={<Login />} /> {/* Route for the login page */}
-          <Route path="/register" element={<Register />} /> {/* Route for the registration page */}
-          <Route path="/jobs" element={<JobList />} /> {/* Route for listing jobs */}
-          <Route path="/jobs/:id" element={<JobDetails />} /> {/* Route for job details, using job ID as a parameter */}
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />{/* Define a route for the root path */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/jobs" element={<JobList />} />
+          <Route path="/jobs/:id" element={<JobDetails />} />
           <Route
             path="/job-seeker/dashboard"
-            element={<PrivateRoute element={<JobSeekerDashboard />} />} {/* Protected route for job seeker dashboard */}
+            element={<PrivateRoute element={<JobSeekerDashboard />} />}
           />
           <Route
             path="/employer/dashboard"
-            element={<PrivateRoute element={<EmployerDashboard />} />} {/* Protected route for employer dashboard */}
+            element={<PrivateRoute element={<EmployerDashboard />} />}
           />
         </Routes>
       </Router>
@@ -35,4 +35,4 @@ const App = () => {
   );
 };
 
-export default App; // Export the App component for use in other parts of the application
+export default App;
