ff92ce106d25244b04c209783e4c95a74bcab457
Elaine Muhombe <emuhombe@gmail.com>
2024-09-21 19:49:11 +0300

    feat: Add Zod validation schema for job category

---
+++ server/src/validators/CategoryValidator.ts
@@ -0,0 +1,11 @@
+//src/validators/CategoryValidatior.ts
+import { z } from 'zod';
+
+// Zod schema for category validation
+const CategorySchemaZod = z.object({
+  name: z.string().min(1, 'Category name is required'),
+  image: z.string().url('Invalid image URL').optional(), // Optional image URL
+});
+
+// Export the validation schema
+export { CategorySchemaZod };
