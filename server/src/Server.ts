dd4ac04ea2ec63de0acf873538bfc92f09cd724c
Elaine Muhombe <emuhombe@gmail.com>
2024-09-21 19:09:45 +0300

    feat: Establish MongoDB connection with Mongoose in Server.ts

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,12 +1,24 @@
-//src/Server.ts
+// src/Server.ts
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { swaggerDocs, swaggerUi } from './config/Swagger'; 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 7100;
+
+// Connect to MongoDB
+const mongoURI = process.env.MONGODB_URI;
+
+mongoose.connect(mongoURI!)
+  .then(() => {
+    console.log('MongoDB connected successfully');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
 
 // Set up Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
