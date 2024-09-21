88e5aae05bfc54f4e0aba845aea534bf598b1e13
Elaine Muhombe <emuhombe@gmail.com>
2024-09-21 19:14:43 +0300

    feat: Implement user validation schema using Zod

---
+++ server/src/validation/UserValidation.ts
@@ -0,0 +1,12 @@
+//src/validation/UserValidation.ts
+import { z } from 'zod';
+
+const UserSchemaZod = z.object({
+  username: z.string().min(1, 'Username is required'),
+  email: z.string().email('Invalid email format').nonempty('Email is required'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+  role: z.enum(['jobSeeker', 'employer']),
+  profile: z.string().optional(),
+});
+
+export default UserSchemaZod;
