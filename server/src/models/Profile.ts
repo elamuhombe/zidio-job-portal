82a203142a8764ad441fbc90004f273650d57d17
Elaine Muhombe <emuhombe@gmail.com>
2024-09-22 20:42:18 +0300

    refactor: Move IProfile interface to index.ts in types directory

--- server/src/models/Profile.ts
+++ server/src/models/Profile.ts
@@ -1,20 +1,8 @@
 //src/models/Profile.ts
 import mongoose, { Schema } from "mongoose";
-import { IUser } from "./User";
+import { IProfile } from "../types";
 
-interface IProfile extends Document {
-    user: IUser['_id'];
-    resume: string; // URL to uploaded resume
-    contactDetails: {
-      phone: string;
-      address: string;
-    };
-    companyInfo?: {
-      name: string;
-      description: string;
-    };
-  }
-  
+
   const ProfileSchema: Schema<IProfile> = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     resume: { type: String },
