e7cc2d1a1c16fdf96ea81b79e4ba1ae714778133
Elaine Muhombe <emuhombe@gmail.com>
2024-09-22 20:44:44 +0300

    feat: Add IUser, IJob, IProfile, and ICategory interfaces in types index.ts

---
+++ server/src/types/index.ts
@@ -0,0 +1,46 @@
+//src/types/index.ts
+
+import mongoose from "mongoose";
+
+//Export User interface
+export interface IUser {
+  _id: mongoose.Types.ObjectId; // Explicitly declare _id field
+  username: string;
+  email: string;
+  password: string;
+  role: "jobSeeker" | "employer" | "admin";
+  profile?: IProfile;
+}
+// Export the Job interface
+export interface IJob {
+  title: string;
+  description: string;
+  qualifications: string[];
+  responsibilities: string[];
+  location: string;
+  salaryRange?: { min: number; max: number };
+  category: ICategory["_id"];
+  company: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+// Export Profile interface
+export interface IProfile {
+  user: IUser["_id"];
+  resume: string;
+  contactDetails: {
+    phone: string;
+    address: string;
+  };
+  companyInfo?: {
+    name: string;
+    description: string;
+  };
+}
+
+//Export Category interface
+export interface ICategory {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  image: string;
+}
