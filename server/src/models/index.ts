266d881cf4f243a7593f68f979b98753c6f2f176
Elaine Muhombe <emuhombe@gmail.com>
2024-09-24 09:22:33 +0300

    refactor: Re-export IUser, User, IProfile, Profile, IJob, Job, IJobApplication, and JobApplication for centralized access

---
+++ server/src/models/index.ts
@@ -0,0 +1,6 @@
+//src/models/index.ts
+
+export {IUser, User} from './User'
+export {IProfile, Profile} from './Profile'
+export {IJob, Job} from './Job'
+export {IJobApplication, JobApplication} from './JobApplication'
